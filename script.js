3e933c1daff4bf7ec91dd4aac3c29c84b82958a0
Phu Duc Nguyen <sillycat@duck.com>
2023-05-29 20:48:56 +0700

    Generate map boundaries

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = window.innerWidth()
-canvas.height = window.innerHeight()
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Boundary {
+    static width = 40
+    static height = 40
+    constructor({ position }) {
+        this.position = position
+        this.width = 40
+        this.height = 40 
+    }
+
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const map = [
+    ['-', '-', '-', '-', '-', '-'],
+    ['-', ' ', ' ', ' ', ' ', '-'],
+    ['-', ' ', '-', '-', ' ', '-'],
+    ['-', ' ', ' ', ' ', ' ', '-'],
+    ['-', '-', '-', '-', '-', '-']
+]
+
+const boundaries = []
+
+map.forEach((row, i) => {
+    console.log(i)
+    row.forEach((symbol, j) => {
+        console.log(j)
+        switch (symbol) {
+            case '-':
+                boundaries.push(
+                    new Boundary({
+                        position: {
+                            x: Boundary.width * j,
+                            y: Boundary.height * i
+                        }
+                    })
+                )
+                break
+            case ' ':
+                break
+        }
+    })
+})
+
+boundaries.forEach(boundary => {
+    boundary.draw()
+})
